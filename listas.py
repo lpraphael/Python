cad62dacad80abe9852a0ce7894e34edba78376b
Raphael Lyra Perazzo <raphael.perazzo@gmail.com>
2023-05-23 08:18:39 -0300

    add novos métodos ao arquivo litas.py

--- listas.py
+++ listas.py
@@ -32,6 +32,87 @@ print('Imprime o comprimento da nova_lista3', len(nova_lista3))
 for i in range(len(nova_lista3)):
     print(nova_lista3[i])
 
+# Métodos e funções para manipular listas
 
+new_list = [1, 3, 12, 8, 2]
 
+# Append: Adiciona elemento ao final da lista
+
+print('Antes do append: ', new_list)
+
+new_list.append(3)
+
+print('Depois do append: ', new_list)
+
+# Insert: adiciona elemento em uma posição específica da lista
+
+new_list.insert(2, 10) # insert(índice, elemento que quero add)
+
+print('Depois do insert: ', new_list)
+
+# Extend: Utilizado para unir duas listas. Pega os elementos da segunda lista e adiciona um a um ao final da primeira lista
+
+new_list.extend(nova_lista3)
+
+print('Depois do Extend: ', new_list)
+
+# pop: Serve para remover o elemento especificado da lista, caso não especifique, remove o ultimo elemento da lista
+
+print('Lista antes de remover elemento', new_list)
+
+new_list.pop(0)
+
+print('Lista após remover elemento na primeira posição: ', new_list)
+
+new_list.pop()
+
+print('Lista após remover último elemento: ', new_list)
+
+# Remove: Utilizado para remover o primeiro elemento específicado da lista
+
+print('Lista antes de remover elemento: ', new_list)
+
+new_list.remove(3)
+
+print('Lista após remover o primeiro elemento específicado: ', new_list)
+
+# Count: Conta quantas vezes o elemento especificado aparece na lista
+
+print('Quantidade de elementos "2" na lista: ', new_list.count(2))
+
+# Index: Retorna o indice do elemento na lista
+
+print('Índice do elemento 8 na lista: ', new_list.index(8))
+
+# Sort: Ordena a lista
+
+new_list2 = [3, 7, 1000, 25, 64, 78, 1, 0]
+
+new_list2.sort()
+
+print('Retorna a lista ordenada de forma crescente: ', new_list2)
+
+# para retornar a lista ordenada de forma decrescente, é necessário utilizar dentro da função sort o parâmetro reverse = true
+
+new_list2.sort(reverse=True)
+
+print('Retorna a lista ordenada de forma decrescente: ', new_list2)
+
+# Funções para Listas
+
+# len: retorna a quantidade total de elementos da lista
+
+print('Quantidade total de elementos da new_list2: ', len(new_list2))
+
+# sum: Soma todos os elementos da lista
+
+print('Valor da soma de todos os elementos da new_list2: ', sum(new_list2))
+
+# max: Retorna o elemento com maior valor da lista
+
+print('Maior valor entre todos os elementos da new_list2: ', max(new_list2))
+
+# min: Retorna o elemento com menor valor da lista
+
+print('Menor valor entre todos os elementos da new_list2: ', min(new_list2))
 
